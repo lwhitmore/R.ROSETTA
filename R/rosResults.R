ad28ba7a97420d0c13ff01e9237e9598c9284291
lwhitmore <leanne382@gmail.com>
2026-01-14 16:17:46 -0800

    remove print statements

--- R/rosResults.R
+++ R/rosResults.R
@@ -23,14 +23,10 @@ rosResults<-function(path, path_logs, roc, index_of_mainclass){
   for(k in 1:length(txt_files_ls)){
         logs=read.table(txt_files_ls[k], fill=T)
         if (index_of_mainclass==0) {
-          print(index_of_mainclass)
-
           p=as.numeric(gsub("%", "", logs[nrow(logs), "V2"]))*.01
           TP <- as.numeric(logs[nrow(logs)-4,"V3"])
           FN <- as.numeric(logs[nrow(logs)-4,"V4"])
         } else { 
-          print ("test")
-          print(index_of_mainclass)
           p=as.numeric(gsub("%", "", logs[nrow(logs), "V3"]))*.01
           TP <- as.numeric(logs[nrow(logs)-3,"V4"])
           FN <- as.numeric(logs[nrow(logs)-3,"V3"])
@@ -47,41 +43,6 @@ rosResults<-function(path, path_logs, roc, index_of_mainclass){
       stats2 <- rbind(stats2, c("MEAN.precision", mean(precision)))
       stats2 <- rbind(stats2, c("MEAN.recall", mean(recall)))
       stats2 <- rbind(stats2, c("MEAN.f1score", mean(f1score)))
-
-
-  ##MCC
-  #cts<-statsTab[which(statsTab$V2=="|"),]
-  #if(length(which(cts$V1=="Undefined"))!=0){
-  #cts<-cts[-which(cts$V1=="Undefined"),]
-  #}
-  #noc<-unique(as.numeric(as.character(cts$V1)))
-
-  #MCC=c()
-
-#for(i in seq(1, dim(cts)[1], by = length(noc))){
-#if(length(noc) == 2){
-  
-  #TP=as.numeric(as.character(cts$V3[i]))
-  #FN=as.numeric(as.character(cts$V3[i+1]))
-  #FP=as.numeric(as.character(cts$V4[i]))
-  #TN=as.numeric(as.character(cts$V4[i+1]))
-  #MCC<-c(MCC,((TP*TN)-(FP*FN))/sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN)))
-    
-#}else{
-  
-  #MCC<-c(MCC,NA)
-  ##in progress##
-  #TP=cts$V3[i]
-  #FN=cts$V3[i+1]
-  #FP=cts$V4[i]
-  #TN=cts$V4[i+1]
-  
-#}
-  #statsMCC=data.frame("MCC.mean",as.factor(mean(MCC)))
-  #colnames(statsMCC)<-c("Measure","Value")
-#}
-  
-  #stats2=rbind(stats2,statsMCC)
   
   return(stats2)
 
